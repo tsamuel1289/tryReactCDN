a556e97ee7c4e14d0202a3b85a3da8246648fa25
tsamuel1289 <romerotitosamuel@gmail.com>
2020-12-23 12:04:15 -0400

    update

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const Link = ReactRouterDOM.Link;
+const Route = ReactRouterDOM.Route;
+import Home from 'Home';
+
+const AppComponent = () => {
+    return <div>
+        <ReactRouterDOM.HashRouter>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
+          </ul>
+
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+
+        </ReactRouterDOM.HashRouter>
+
+    </div>
+   
+};
+
+        
+        const Login = () => <h1>Login</h1>
+        const Register = () => <h1>Register 2</h1>
+
+
+ReactDOM.render(<AppComponent/>, document.getElementById('root'));
+
